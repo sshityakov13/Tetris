478f29d224a56b21facf8f29d174e3b3362076ab
Сергей Шитяков <spshityakov@edu.hse.ru>
2019-12-19 19:04:00 +0300

    аапукп

--- _Tetris_/Figure.cs
+++ _Tetris_/Figure.cs
@@ -4,6 +4,22 @@ using System.Text;
 
 namespace _Tetris_
 {
+    struct L
+    {
+        public int i;
+        public int j;
+    }
+
+    struct StartPosition
+    {
+        public int I;
+        public int J;
+    }
+
+    enum FigureLocation
+    {
+        Up, Right, Down, Left
+    }
     class Figure
     {
     }
